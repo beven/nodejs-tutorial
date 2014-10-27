ad5db56914c45cedd508338092db43a1d4ac4cc2
Beven Lee <bevenl@gmail.com>
2014-10-28 00:18:56 +1100

    Refactored form creation to a different module

---
+++ src/formFactory.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Beven on 27/10/2014.
+ */
+
+function createUploadForm() {
+    var body = '<html>'+
+        '<head>'+
+        '<meta http-equiv="Content-Type" content="text/html; '+
+        'charset=UTF-8" />'+
+        '</head>'+
+        '<body>'+
+        '<form action="/upload" method="post">'+
+        '<textarea name="text" rows="20" cols="60"></textarea>'+
+        '<input type="submit" value="Submit text" />'+
+        '</form>'+
+        '</body>'+
+        '</html>';
+    return body;
+}
+
+exports.createUploadForm = createUploadForm;
--- src/requestHandlers.js
+++ src/requestHandlers.js
@@ -3,21 +3,11 @@
  */
 
 var querystring = require("querystring");
+var formFactory = require("./formFactory");
 
 function start(response, postData) { // this is ugly - variable not used in signature
     console.log("Request handler 'start' was called.");
-    var body = '<html>'+
-        '<head>'+
-        '<meta http-equiv="Content-Type" content="text/html; '+
-        'charset=UTF-8" />'+
-        '</head>'+
-        '<body>'+
-        '<form action="/upload" method="post">'+
-        '<textarea name="text" rows="20" cols="60"></textarea>'+
-        '<input type="submit" value="Submit text" />'+
-        '</form>'+
-        '</body>'+
-        '</html>';
+    var body = formFactory.createUploadForm();
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write(body);
     response.end();
